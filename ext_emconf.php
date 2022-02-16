673a8f89c64ad51a0cc83dc2da52620f617eb710
Michael Schams <typo3@2020.schams.net>
2022-02-16 11:55:27 +1100

    [TASK] Raise TYPO3 and PHP version dependencies
    - TYPO3 v10 and v11
    - PHP v7.2, v7.3, v7.4, and v8.0, v8.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -27,8 +27,8 @@ $EM_CONF[$_EXTKEY] = [
     ],
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-10.4.99',
-            'php' => '7.2.0-7.4.99'
+            'typo3' => '10.4.0-11.5.99',
+            'php' => '7.2.0-8.1.99'
         ],
         'conflicts' => [],
         'suggests' => []
